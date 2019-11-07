1c4c3390056d0788b9f40383b7c07f1847eb0361
Anastasia Tsegelskaya <muaaaaaaa@mail.ru>
2019-11-07 11:34:44 +0300

    change stations ids to numbers

--- src/map/constants/lines.js
+++ src/map/constants/lines.js
@@ -7,52 +7,52 @@ export const LINES = [
         color: '#ED0004',
         stations: [
             {
-                id: 'red_1',
+                id: 1,
                 name: 'Девяткино',
                 x: 556,
                 y: 124
             },{
-                id: 'red_2',
+                id: 2,
                 name: 'Гражданский проспект',
                 x: 556,
                 y: 166
             },{
-                id: 'red_3',
+                id: 3,
                 name: 'Академическая',
                 x: 556,
                 y: 222
             },{
-                id: 'red_4',
+                id: 4,
                 name: 'Политехническая',
                 x: 556,
                 y: 265
             },{
-                id: 'red_5',
+                id: 5,
                 name: 'Площадь Мужества',
                 x: 556,
                 y: 307
             },{
-                id: 'red_6',
+                id: 6,
                 name: 'Лесная',
                 x: 556,
                 y: 350
             },{
-                id: 'red_7',
+                id: 7,
                 name: 'Выборгская',
                 x: 556,
                 y: 393
             },{
-                id: 'red_8',
+                id: 8,
                 name: 'Площадь Ленина',
                 x: 556,
                 y: 435
             },{
-                id: 'red_9',
+                id: 9,
                 name: 'Чернышевская',
                 x: 556,
                 y: 552
             },{
-                id: 'red_10',
+                id: 10,
                 name: 'Площадь Восстания',
                 x: 556,
                 y: 606,
@@ -70,7 +70,7 @@ export const LINES = [
                 x: 550,
                 y: 712
             },{
-                id: 'red_11',
+                id: 11,
                 name: 'Владимирская',
                 x: 540,
                 y: 722,
@@ -78,23 +78,23 @@ export const LINES = [
                 textPosition: 'bottomRight',
                 extraX: 3
             },{
-                id: 'red_12',
+                id: 12,
                 name: 'Пушкинская',
                 x: 432,
                 y: 785,
                 transfer: true,
                 extraY: -2
             },{
-                id: 'red_13',
+                id: 13,
                 name: 'Технологический институт',
                 x: 331,
                 y: 844,
                 transfer: true,
                 textPosition: 'topLeft',
                 extraY: 20,
-                checkConnectors: ['blue_11']
+                checkConnectors: [30]
             },{
-                id: 'red_14',
+                id: 14,
                 name: 'Балтийская',
                 x: 223,
                 y: 906,
@@ -115,27 +115,27 @@ export const LINES = [
                 x: 167,
                 y: 954
             },{
-                id: 'red_15',
+                id: 15,
                 name: 'Нарвская',
                 x: 166,
                 y: 963
             },{
-                id: 'red_16',
+                id: 16,
                 name: 'Кировский завод',
                 x: 166,
                 y: 1003
             },{
-                id: 'red_17',
+                id: 17,
                 name: 'Автово',
                 x: 166,
                 y: 1058
             },{
-                id: 'red_18',
+                id: 18,
                 name: 'Ленинский проспект',
                 x: 166,
                 y: 1098
             },{
-                id: 'red_19',
+                id: 19,
                 name: 'Проспект Ветеранов',
                 x: 166,
                 y: 1152
@@ -148,43 +148,43 @@ export const LINES = [
         color: '#3A70BB',
         stations: [
             {
-                id: 'blue_1',
+                id: 20,
                 name: 'Парнас',
                 x: 404,
                 y: 124
             },
             {
-                id: 'blue_2',
+                id: 21,
                 name: 'Проспект Просвящения',
                 x: 404,
                 y: 156
             },
             {
-                id: 'blue_3',
+                id: 22,
                 name: 'Озерки',
                 x: 404,
                 y: 202
             },
             {
-                id: 'blue_4',
+                id: 23,
                 name: 'Удельная',
                 x: 404,
                 y: 234
             },
             {
-                id: 'blue_5',
+                id: 24,
                 name: 'Пионерская',
                 x: 404,
                 y: 267
             },
             {
-                id: 'blue_6',
+                id: 25,
                 name: 'Черная речка',
                 x: 404,
                 y: 299
             },
             {
-                id: 'blue_7',
+                id: 26,
                 name: 'Петроградская',
                 x: 404,
                 y: 398
@@ -200,7 +200,7 @@ export const LINES = [
                 y: 468
             },
             {
-                id: 'blue_8',
+                id: 27,
                 name: 'Горьковская',
                 x: 415,
                 y: 482,
@@ -222,7 +222,7 @@ export const LINES = [
                 y: 531
             },
             {
-                id: 'blue_9',
+                id: 28,
                 name: 'Невский проспект',
                 x: 448,
                 y: 606,
@@ -248,7 +248,7 @@ export const LINES = [
                 y: 684
             },
             {
-                id: 'blue_10',
+                id: 29,
                 name: 'Сенная площадь',
                 x: 368,
                 y: 722,
@@ -272,52 +272,52 @@ export const LINES = [
                 y: 755
             },
             {
-                id: 'blue_11',
+                id: 30,
                 name: 'Технологический институт',
                 x: 339,
                 y: 828,
                 transfer: true,
                 hideText: true,
-                checkConnectors: ['red_13']
+                checkConnectors: [13]
             },
             {
-                id: 'blue_12',
+                id: 31,
                 name: 'Фрунзенская',
                 x: 339,
                 y: 907
             },
             {
-                id: 'blue_13',
+                id: 32,
                 name: 'Московские Ворота',
                 x: 339,
                 y: 947
             },
             {
-                id: 'blue_14',
+                id: 33,
                 name: 'Электросила',
                 x: 339,
                 y: 990
             },
             {
-                id: 'blue_15',
+                id: 34,
                 name: 'Парк Победы',
                 x: 339,
                 y: 1028
             },
             {
-                id: 'blue_16',
+                id: 35,
                 name: 'Московская',
                 x: 339,
                 y: 1070
             },
             {
-                id: 'blue_17',
+                id: 36,
                 name: 'Звездная',
                 x: 339,
                 y: 1111
             },
             {
-                id: 'blue_18',
+                id: 37,
                 name: 'Купчино',
                 x: 339,
                 y: 1152
@@ -330,13 +330,13 @@ export const LINES = [
         color: '#329244',
         stations: [
             // {
-            //     id: 'green_11',
+            //     id: 38,
             //     name: 'Беговая',
             //     x: 102,
             //     y: 298
             // },
             // {
-            //     id: 'green_12',
+            //     id: 39,
             //     name: 'Новокрестовская',
             //     x: 102,
             //     y: 393
@@ -352,14 +352,14 @@ export const LINES = [
             //     y: 498
             // },
             {
-                id: 'green_1',
+                id: 40,
                 name: 'Приморская',
                 x: 114,
                 y: 513,
                 textPosition: 'bottomLeft'
             },
             {
-                id: 'green_2',
+                id: 41,
                 name: 'Василеостровская',
                 x: 187,
                 y: 586,
@@ -381,7 +381,7 @@ export const LINES = [
                 y: 620
             },
             {
-                id: 'green_3',
+                id: 42,
                 name: 'Гостиный Двор',
                 x: 459,
                 y: 620,
@@ -391,7 +391,7 @@ export const LINES = [
                 extraY: 17
             },
             {
-                id: 'green_4',
+                id: 43,
                 name: 'Маяковская',
                 x: 567,
                 y: 620,
@@ -415,7 +415,7 @@ export const LINES = [
                 y: 650
             },
             {
-                id: 'green_5',
+                id: 44,
                 name: 'Площадь Александра Невского',
                 x: 685,
                 y: 694,
@@ -423,34 +423,34 @@ export const LINES = [
                 textPosition: 'topRight',
                 textWidth: 90,
                 extraY: 22,
-                checkConnectors: ['orange_4']
+                checkConnectors: [55]
             },
             {
-                id: 'green_6',
+                id: 45,
                 name: 'Елизаровская',
                 x: 685,
                 y: 878
             },
             {
-                id: 'green_7',
+                id: 46,
                 name: 'Ломоносовкая',
                 x: 685,
                 y: 946
             },
             {
-                id: 'green_8',
+                id: 47,
                 name: 'Пролетарская',
                 x: 685,
                 y: 1014
             },
             {
-                id: 'green_9',
+                id: 48,
                 name: 'Обухово',
                 x: 685,
                 y: 1083
             },
             {
-                id: 'green_10',
+                id: 49,
                 name: 'Рыбацкое',
                 x: 685,
                 y: 1151
@@ -464,7 +464,7 @@ export const LINES = [
         hideStartNumber: true,
         stations: [
             // {
-            //     id: 'orange_9',
+            //     id: 50,
             //     name: 'Горный институт',
             //     x: 122,
             //     y: 679,
@@ -482,7 +482,7 @@ export const LINES = [
             //     y: 707
             // },
             // {
-            //     id: 'orange_10',
+            //     id: 51,
             //     name: 'Театральная',
             //     x: 258,
             //     y: 707,
@@ -491,7 +491,7 @@ export const LINES = [
             //     extraX: -4
             // },
             {
-                id: 'orange_1',
+                id: 52,
                 name: 'Спасская',
                 x: 359,
                 y: 707,
@@ -502,7 +502,7 @@ export const LINES = [
                 extraY: 7
             },
             {
-                id: 'orange_2',
+                id: 53,
                 name: 'Достоевская',
                 x: 531,
                 y: 707,
@@ -513,7 +513,7 @@ export const LINES = [
                 extraX: -15
             },
             {
-                id: 'orange_3',
+                id: 54,
                 name: 'Лиговский проспект',
                 x: 624,
                 y: 707,
@@ -523,16 +523,16 @@ export const LINES = [
                 extraY: 32
             },
             {
-                id: 'orange_4',
+                id: 55,
                 name: 'Площадь Александра Невского',
                 x: 697,
                 y: 707,
                 transfer: true,
                 hideText: true,
-                checkConnectors: ['green_5']
+                checkConnectors: [44]
             },
             {
-                id: 'orange_5',
+                id: 56,
                 name: 'Новочеркасская',
                 x: 809,
                 y: 707,
@@ -561,19 +561,19 @@ export const LINES = [
                 y: 735
             },
             {
-                id: 'orange_6',
+                id: 57,
                 name: 'Ладожская',
                 x: 859,
                 y: 748
             },
             {
-                id: 'orange_7',
+                id: 58,
                 name: 'Проспект Большевиков',
                 x: 859,
                 y: 805
             },
             {
-                id: 'orange_8',
+                id: 59,
                 name: 'Улица Дыбенко',
                 x: 859,
                 y: 877
@@ -586,31 +586,31 @@ export const LINES = [
         color: '#652C91',
         stations: [
             {
-                id: 'purple_1',
+                id: 60,
                 name: 'Комендантский проспект',
                 x: 253,
                 y: 253
             },
             {
-                id: 'purple_2',
+                id: 61,
                 name: 'Старая Деревня',
                 x: 253,
                 y: 299
             },
             {
-                id: 'purple_3',
+                id: 62,
                 name: 'Крестовский Остров',
                 x: 253,
                 y: 384
             },
             {
-                id: 'purple_4',
+                id: 63,
                 name: 'Чкаловская',
                 x: 253,
                 y: 448
             },
             {
-                id: 'purple_5',
+                id: 64,
                 name: 'Спортивная',
                 x: 253,
                 y: 489
@@ -636,7 +636,7 @@ export const LINES = [
                 y: 584
             },
             {
-                id: 'purple_6',
+                id: 65,
                 name: 'Адмиралтейская',
                 x: 318,
                 y: 649,
@@ -654,7 +654,7 @@ export const LINES = [
                 y: 696
             },
             {
-                id: 'purple_7',
+                id: 66,
                 name: 'Садовая',
                 x: 350,
                 y: 722,
@@ -664,7 +664,7 @@ export const LINES = [
                 extraX: 3
             },
             {
-                id: 'purple_8',
+                id: 67,
                 name: 'Звенигородская',
                 x: 432,
                 y: 802,
@@ -672,7 +672,7 @@ export const LINES = [
                 extraY: -1
             },
             {
-                id: 'purple_9',
+                id: 68,
                 name: 'Обводный канал',
                 x: 491,
                 y: 863,
@@ -689,37 +689,37 @@ export const LINES = [
                 y: 889
             },
             {
-                id: 'purple_10',
+                id: 69,
                 name: 'Волковская',
                 x: 512,
                 y: 907
             },
             {
-                id: 'purple_11',
+                id: 70,
                 name: 'Бухарестская',
                 x: 512,
                 y: 956
             },
             {
-                id: 'purple_12',
+                id: 71,
                 name: 'Международная',
                 x: 512,
                 y: 1005
             },
             // {
-            //     id: 'purple_13',
+            //     id: 72,
             //     name: 'Проспект Славы',
             //     x: 512,
             //     y: 1054
             // },
             // {
-            //     id: 'purple_14',
+            //     id: 73,
             //     name: 'Дунайская',
             //     x: 512,
             //     y: 1104
             // },
             // {
-            //     id: 'purple_15',
+            //     id: 74,
             //     name: 'Шушары',
             //     x: 512,
             //     y: 1151
@@ -729,13 +729,13 @@ export const LINES = [
 ];
 
 export const CONNECTORS = [
-    ['green_3', 'blue_9'],
-    ['green_4', 'red_10'],
-    ['orange_1', 'purple_7', 'blue_10'],
-    ['orange_2', 'red_11'],
-    ['orange_4', 'green_5'],
-    ['red_12', 'purple_8'],
-    ['red_13', 'blue_11']
+    [28, 42],
+    [10, 43],
+    [29, 52, 66],
+    [44, 55],
+    [13, 30],
+    [11, 53],
+    [12, 67]
 ].map(connector => flatten(connector.map(id => {
     const station = find(propEq('id', id), flatten(LINES.map(line => line.stations)));
     return [station.x, station.y];
